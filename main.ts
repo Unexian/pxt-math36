44d5c665fe1de3dc1ad24adb3db67c0a53ff6ebd
Nif <73426353+Unexian@users.noreply.github.com>
2022-12-22 18:41:45 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+function angleBetween(sprite1: Sprite, sprite2: Sprite) {
+    let deltaX = sprite2.x - sprite1.x
+    let deltaY = sprite2.y - sprite1.y
+    return Math.atan2(deltaX, deltaY)
+}
+function pol2Rect(r: number, theta: number) {
+    return {x: r * Math.cos(theta), y: r * Math.sin(theta)}
+}
