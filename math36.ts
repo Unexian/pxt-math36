e0823112b437759ffde033a3cebcb7b0c7b30301
Nif <73426353+Unexian@users.noreply.github.com>
2022-12-22 19:04:41 +0000

    Update math36.ts

--- math36.ts
+++ math36.ts
@@ -17,18 +17,12 @@ namespace Math {
         let deltaY = sprite2.y - sprite1.y
         return Math.sqrt(deltaY * deltaY + deltaX * deltaX)
     }
-    //% block="Polar r=$r θ=$theta to rectangular, get $getX"
-    //% getX.shadow = "toggleYX"
-    export function pol2Rect(r: number, theta: number, getX?: boolean) {
-        if (typeof (getX) !== 'undefined') { return { x: r * Math.cos(theta), y: r * Math.sin(theta) } }
-        else if (getX) { return r * Math.cos(theta) }
-        else { return r * Math.sin(theta) }
+    //% block="Polar r=$r|θ=$theta|to rectangular"
+    export function pol2Rect(r: number, theta: number) {
+        return { x: r * Math.cos(theta), y: r * Math.sin(theta) }
     }
-    //% block="Polar r=$r θ=$theta to rectangular, get $getR"
-    //% getR.shadow = "toggleThetaR"
-    export function rect2Pol(x: number, y: number, getR?: boolean) {
-        if (typeof (getR) !== 'undefined') { return { r: Math.sqrt(x * x + y * y), theta: Math.atan2(y, x) } }
-        else if (getR) { return Math.sqrt(x * x + y * y) }
-        else { return Math.atan2(y, x) }
+    //% block="Rectangular x=$x y=$y to polar"
+    export function rect2Pol(x: number, y: number) {
+        return { r: Math.sqrt(x * x + y * y), theta: Math.atan2(y, x) }
     }
 }
