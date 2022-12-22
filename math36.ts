36f75ffad605373521641589138c9a3860f1e7c1
Nif <73426353+Unexian@users.noreply.github.com>
2022-12-22 19:06:43 +0000

    Update math36.ts

--- math36.ts
+++ math36.ts
@@ -5,6 +5,17 @@
 
 //% color="#9b61e2" weight=100
 namespace Math {
+    //% block="Polar r=$r|θ=$theta|to rectangular"
+    export function pol2Rect(r: number, theta: number) {
+        return { x: r * Math.cos(theta), y: r * Math.sin(theta) }
+    }
+    //% block="Rectangular x=$x y=$y to polar"
+    export function rect2Pol(x: number, y: number) {
+        return { r: Math.sqrt(x * x + y * y), theta: Math.atan2(y, x) }
+    }
+}
+//% color="#567ae4" weight=100
+namespace Sprites {
     //% block="Angle between $sprite1 and $sprite2"
     export function angleBetween(sprite1: Sprite, sprite2: Sprite) {
         let deltaX = sprite2.x - sprite1.x
@@ -16,13 +27,5 @@ namespace Math {
         let deltaX = sprite2.x - sprite1.x
         let deltaY = sprite2.y - sprite1.y
         return Math.sqrt(deltaY * deltaY + deltaX * deltaX)
-    }
-    //% block="Polar r=$r|θ=$theta|to rectangular"
-    export function pol2Rect(r: number, theta: number) {
-        return { x: r * Math.cos(theta), y: r * Math.sin(theta) }
-    }
-    //% block="Rectangular x=$x y=$y to polar"
-    export function rect2Pol(x: number, y: number) {
-        return { r: Math.sqrt(x * x + y * y), theta: Math.atan2(y, x) }
     }
 }
